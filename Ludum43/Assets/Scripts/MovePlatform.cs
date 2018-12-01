2a7e7d0d9bc3d0d5c662dbf87bf620f3eb37f01d
grigoriy <gri-bel@yandex.ru>
2018-12-01 15:43:36 +0300

    made player & platform

---
+++ Ludum43/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovePlatform : MonoBehaviour {
+    public Vector3 destinationDistance = new Vector3(0f, 1f, 0f);
+    public float smoothing;
+    public GameObject platform;
+
+    private bool Check = false;
+    private Vector3 startPos;
+    private Vector3 destinationPoint;
+
+    private void Start()
+    {
+        startPos = new Vector3(platform.transform.position.x, platform.transform.position.y, 0);
+        destinationPoint = startPos - destinationDistance;
+    }
+
+    void Update()
+    {
+        Debug.Log(Check);
+        if (Check == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, destinationPoint, smoothing * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, startPos, smoothing * Time.deltaTime);
+        }
+        if (platform.transform.position.y < (destinationPoint - new Vector3(0f, -0.05f, 0f)).y)
+        {
+            Check = true;
+        }
+        if (platform.transform.position.y > (startPos - new Vector3(0f, 0.05f, 0f)).y)
+        {
+            Check = false;
+        }
+    }
+}
---
+++ Ludum43/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+/*todo: 
+ bool is_jump
+ ontriggerenter2d teleport + catch
+ throw
+ teleport
+ move    
+     */
+public class PlayerControl : MonoBehaviour
+{
+    //init variables
+    public GameObject target; //also an error here.
+    public Rigidbody2D Rb2d;
+    public BoxCollider2D Bc2d;
+    private float acceleration;
+    private float lastVelocity;
+    public Vector2 Speed = new Vector3(0f,0f,0f);
+    private bool is_jump = false;
+    public Vector2 jump = new Vector2(0f, 2f);
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Move(Vector2 speed)//move через rigidbody
+    {
+        Rb2d.velocity = new Vector2(speed.x, Rb2d.velocity.y); 
+    }
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        position.y -= 1.5f;
+        Vector2 direction = Vector2.down;
+        float distance = 0.01f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = (Rb2d.velocity.x - lastVelocity) / Time.fixedDeltaTime;
+        lastVelocity = Rb2d.velocity.x;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (is_jump == true)
+            {
+                is_jump = false;
+                Rb2d.AddForce(jump);
+            }
+            else if (IsGrounded())
+            {
+                Rb2d.AddForce(jump);
+                is_jump = true;
+            }
+        }
+        if (Input.GetKey(KeyCode.D)) //when Right Key pressed (D)
+        {
+            Move(Speed); 
+        }
+        if (Input.GetKey(KeyCode.A)) //when Right Key pressed (D)
+        {
+            Move(-Speed);
+        }
+        if (Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.D)) //when Left Key pressed (A)
+        {
+            Rb2d.velocity = new Vector2(0.0f,Rb2d.velocity.y);
+        }
+       
+    }
+}
